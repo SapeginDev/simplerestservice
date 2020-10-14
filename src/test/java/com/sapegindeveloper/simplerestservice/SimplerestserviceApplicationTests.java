a14a41ae8372a609bf941b16a3f88e3d65f08086
sapeginDev <ivan260580>
2020-10-14 21:53:05 +1000

    Adding Test

--- src/test/java/com/sapegindeveloper/simplerestservice/SimplerestserviceApplicationTests.java
+++ src/test/java/com/sapegindeveloper/simplerestservice/SimplerestserviceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.sapegindeveloper.simplerestservice;
 
+import com.sapegindeveloper.simplerestservice.controller.SimpleController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SimplerestserviceApplicationTests {
 
     @Test
     void contextLoads() {
+        SimpleController restController = new SimpleController();
+        String result = restController.getStringOk();
+        assertEquals(result, "Ok!!!");
     }
 
 }
